06561154ba172bd1b23c6e2877b102be087bf303
Andras Gaal <andras@gaal.eu>
2025-04-23 10:54:07 +0200

    add readme, linting

--- src/devices/OnOffSwitch.ts
+++ src/devices/OnOffSwitch.ts
@@ -5,15 +5,7 @@ import { OnOffDevice } from './OnOffDevice.js';
 class OnOffSwitch extends OnOffDevice {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(structureSection: any, platform: LoxonePlatform) {
-    // eslint-disable-next-line prettier/prettier
-    super(
-      structureSection, 
-      platform, 
-      OnOffSwitch.name,
-      'switch',
-      structureSection.states.active,
-      onOffSwitch
-    );
+    super(structureSection, platform, OnOffSwitch.name, 'switch', structureSection.states.active, onOffSwitch);
   }
 }
 
